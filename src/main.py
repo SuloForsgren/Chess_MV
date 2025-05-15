bbb5501613d9d46e5840eb6f3e01620b4e3ba6ad
SuloForsgren <sulo.forsgren@gmail.com>
2025-05-16 00:02:30 +0300

    Split to functions

--- src/main.py
+++ src/main.py
@@ -15,58 +15,65 @@ def order_corners(pts):
 
     return rect
 
-largest_square = None
+def preprocess_image(board_img) :
+    img = cv.imread(board_img)
+    img = cv.resize(img, (800, int(img.shape[0] * 800 / img.shape[1])))
 
-board_img = "img/my2.jpg"
-img = cv.imread(board_img)
-img = cv.resize(img, (800, int(img.shape[0] * 800 / img.shape[1])))
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray_img, (5,5), 0)
+    edges = cv.Canny(blur, 50, 150)
 
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-blur = cv.GaussianBlur(gray_img, (5,5), 0)
-edges = cv.Canny(blur, 50, 150)
+    resized_image = cv.resize(edges, (600,600))
+    cv.imshow("Detected Chessboard", resized_image)
 
-#resized_image = cv.resize(edges, (600,600))
-#cv.imshow("Detected Chessboard", resized_image)
-#cv.waitKey(0)
+    return img, edges
 
-contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+def main() :
+    largest_square = None
 
-largest_square = max(contours, key=cv.contourArea)
+    board_img = "img/origin.jpg"
+    img, edges = preprocess_image(board_img)
 
-rect = cv.minAreaRect(largest_square)
-box = cv.boxPoints(rect)
-box = box.astype("float32")
+    contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-ordered_box = order_corners(box)
+    largest_square = max(contours, key=cv.contourArea)
 
-widthA = np.linalg.norm(ordered_box[2] - ordered_box[3])
-widthB = np.linalg.norm(ordered_box[1] - ordered_box[0])
-maxWidth = max(int(widthA), int(widthB))
+    rect = cv.minAreaRect(largest_square)
+    box = cv.boxPoints(rect)
+    box = box.astype("float32")
 
-heightA = np.linalg.norm(ordered_box[1] - ordered_box[2])
-heightB = np.linalg.norm(ordered_box[0] - ordered_box[3])
-maxHeight = max(int(heightA), int(heightB))
+    ordered_box = order_corners(box)
 
-dst_corners = np.array([
-    [0, 0],  # Top-left corner
-    [maxWidth - 1, 0],  # Top-right corner
-    [maxWidth - 1, maxHeight - 1],  # Bottom-right corner
-    [0, maxHeight - 1]  # Bottom-left corner
-], dtype="float32")
+    widthA = np.linalg.norm(ordered_box[2] - ordered_box[3])
+    widthB = np.linalg.norm(ordered_box[1] - ordered_box[0])
+    maxWidth = max(int(widthA), int(widthB))
 
-img_with_contours = edges.copy()
+    heightA = np.linalg.norm(ordered_box[1] - ordered_box[2])
+    heightB = np.linalg.norm(ordered_box[0] - ordered_box[3])
+    maxHeight = max(int(heightA), int(heightB))
 
-matrix = cv.getPerspectiveTransform(ordered_box, dst_corners)
-warped = cv.warpPerspective(img, matrix, (maxWidth, maxHeight))
+    dst_corners = np.array([
+        [0, 0],  # Top-left corner
+        [maxWidth - 1, 0],  # Top-right corner
+        [maxWidth - 1, maxHeight - 1],  # Bottom-right corner
+        [0, maxHeight - 1]  # Bottom-left corner
+    ], dtype="float32")
 
-img_contours_gray = img.copy()
-#cv.drawContours(img_contours_gray, contours, -1, (255, 0, 0), 2)
-cv.polylines(img_contours_gray, [box.astype(int)], isClosed=True, color=(0, 255, 0), thickness=3)
+    img_with_contours = edges.copy()
 
-cv.imshow("Detected Chessboard Corners", img_contours_gray)
-cv.imshow("Warped Chessboard", warped)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    matrix = cv.getPerspectiveTransform(ordered_box, dst_corners)
+    warped = cv.warpPerspective(img, matrix, (maxWidth, maxHeight))
 
-print("Chessboard corner points (ordered):")
-print(ordered_box)
+    img_contours_gray = img.copy()
+    cv.polylines(img_contours_gray, [box.astype(int)], isClosed=True, color=(0, 255, 0), thickness=3)
+
+    cv.imshow("Detected Chessboard Corners", img_contours_gray)
+    cv.imshow("Warped Chessboard", warped)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    print("Chessboard corner points (ordered):")
+    print(ordered_box)
+
+if __name__ == "__main__" :
+    main()
