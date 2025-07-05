a68ad8dc0a4e8fabf071249a1ea7c0ff2c2dc838
SuloForsgren <sulo.forsgren@gmail.com>
2025-07-05 16:21:35 +0300

    Blur check + transfer to piece detection

--- src/chess_detection.py
+++ src/chess_detection.py
@@ -4,17 +4,21 @@ class chess_logic:
     def __init__(self):
         self.board = chess.Board()
         
-    def check_board(self, piece_array):
-        for i in range(64):
-            piece = self.board.piece_at(i)
-            if bool(piece) != piece_array[i]:
-                return False
-        return True
+    #def check_board(self, piece_array):
+    #    for i in range(64):
+    #        piece = self.board.piece_at(i)
+    #        if bool(piece) != piece_array[i]:
+    #            return False
+    #    return True
 
-    def print_board_presence(self, piece_array):
-        # chess.square_name(i) gives you 'a1', 'b1', … 'h8'
-        for i in range(64):
-            sq = chess.square_name(i)
-            has_piece = piece_array[i]
-            print(f"{sq}: {'●' if has_piece else '·'}")
-        print()  # blank line at end
+    #def print_board_presence(self, piece_array):
+    #    # chess.square_name(i) gives you 'a1', 'b1', … 'h8'
+    #    for i in range(64):
+    #       sq = chess.square_name(i)
+    #        has_piece = piece_array[i]
+    #        print(f"{sq}: {'●' if has_piece else '·'}")
+    #    print()  # blank line at end
+
+
+    def check_board(self) :
+        print (self.board)
